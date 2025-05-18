4300e851df8be667259109930c14d076c9042c9e
liuyi <liuyiinhhu@gmail.com>
2025-05-18 23:26:38 +0800

    add db

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,4 +1,13 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({})
-export class DatabaseModule {}
+export class DatabaseModule {
+    static forRoot(configRegister: () => TypeOrmModuleOptions): DynamicModule {
+        return {
+            global: true,
+            module: DatabaseModule,
+            imports: [TypeOrmModule.forRoot(configRegister())],
+        };
+    }
+}
