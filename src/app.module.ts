fd6631aba63e86fd93f3cb78c6614a634b585c12
liuyi <liuyiinhhu@gmail.com>
2025-05-18 21:06:51 +0800

    add new module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContentModule } from './modules/content/content.module';
+import { CoreModule } from './modules/core/core.module';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-    imports: [ContentModule],
+    imports: [ContentModule, CoreModule, DatabaseModule],
     controllers: [AppController],
     providers: [AppService],
 })
