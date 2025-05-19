34c4ac8f8678cd139ec6019c16cefac3b07b10b5
liuyi <liuyiinhhu@gmail.com>
2025-05-19 11:12:36 +0800

    add db

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,5 +1,9 @@
-import { DynamicModule, Module } from '@nestjs/common';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DynamicModule, Module, Provider, Type } from '@nestjs/common';
+import { getDataSourceToken, TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+import { DataSource, ObjectType } from 'typeorm';
+
+import { CUSTOM_REPOSITORY_METADATA } from '@/modules/database/constants';
 
 @Module({})
 export class DatabaseModule {
@@ -8,6 +12,31 @@ export class DatabaseModule {
             global: true,
             module: DatabaseModule,
             imports: [TypeOrmModule.forRoot(configRegister())],
+        };
+    }
+    static forRepository<T extends Type<any>>(
+        repositories: T[],
+        datasourceName?: string,
+    ): DynamicModule {
+        const providers: Provider[] = [];
+        for (const Repository of repositories) {
+            const entity = Reflect.getMetadata(CUSTOM_REPOSITORY_METADATA, Repository);
+            if (!entity) {
+                continue;
+            }
+            providers.push({
+                inject: [getDataSourceToken(datasourceName)],
+                provide: Repository,
+                useFactory: (datasource: DataSource): InstanceType<typeof Repository> => {
+                    const base = datasource.getRepository<ObjectType<any>>(entity);
+                    return new Repository(base.target, base.manager, base.queryRunner);
+                },
+            });
+        }
+        return {
+            exports: providers,
+            module: DatabaseModule,
+            providers,
         };
     }
 }
