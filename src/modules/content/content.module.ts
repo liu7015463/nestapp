06f187b74250ee282cccbcf8f94c5cf90f83939e
liuyi <liuyiinhhu@gmail.com>
2025-05-19 15:10:56 +0800

    add content

--- src/modules/content/content.module.ts
+++ src/modules/content/content.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { PostEntity } from '@/modules/content/entities/post.entity';
+import { PostRepository } from '@/modules/content/repositories/post.repository';
+import { SanitizeService } from '@/modules/content/services/SanitizeService';
 import { PostService } from '@/modules/content/services/post.service';
+
+import { PostSubscriber } from '@/modules/content/subscribers/post.subscriber';
+import { DatabaseModule } from '@/modules/database/database.module';
 
 import { PostController } from './controllers/post.controller';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([PostEntity]),
+        DatabaseModule.forRepository([PostRepository]),
+    ],
     controllers: [PostController],
-    providers: [PostService],
-    exports: [PostService],
+    providers: [PostService, PostSubscriber, SanitizeService],
+    exports: [PostService, DatabaseModule.forRepository([PostRepository])],
 })
 export class ContentModule {}
