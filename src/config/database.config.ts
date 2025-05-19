d264b515a1a31e4ddcb10c56b06eb1cbabae5a6c
liuyi <liuyiinhhu@gmail.com>
2025-05-19 09:48:45 +0800

    add db

---
+++ src/config/database.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const database = (): TypeOrmModuleOptions => ({
+    charset: 'utf8mb4',
+    logging: ['error'],
+    type: 'mysql',
+    host: '192.168.50.26',
+    port: 3306,
+    username: '3r',
+    password: '12345678',
+    database: '3r',
+});
