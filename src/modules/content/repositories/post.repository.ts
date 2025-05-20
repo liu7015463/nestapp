fb5b6c646c79181f328b9f8ede38ba6f7e5e563a
liuyi <liuyiinhhu@gmail.com>
2025-05-20 21:49:34 +0800

    add relations

--- src/modules/content/repositories/post.repository.ts
+++ src/modules/content/repositories/post.repository.ts
@@ -1,11 +1,21 @@
 import { Repository } from 'typeorm';
 
+import { CommentEntity } from '@/modules/content/entities/comment.entity';
 import { PostEntity } from '@/modules/content/entities/post.entity';
 import { CustomRepository } from '@/modules/database/decorators/repository.decorator';
 
 @CustomRepository(PostEntity)
 export class PostRepository extends Repository<PostEntity> {
     buildBaseQB() {
-        return this.createQueryBuilder('post');
+        return this.createQueryBuilder('post')
+            .leftJoinAndSelect('post.category', 'category')
+            .leftJoinAndSelect('post.tags', 'tags')
+            .addSelect((query) => {
+                return query
+                    .select('COUNT(c.id)', 'count')
+                    .from(CommentEntity, 'c')
+                    .where('c.post.id = post.id');
+            }, 'commentCount')
+            .loadRelationCountAndMap('post.commentCOunt', 'post.comments');
     }
 }
