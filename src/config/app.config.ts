2afa6bcb4c2d192c45e18e8e6037ef88e6b0ce80
liuyi <liuyiinhhu@gmail.com>
2025-06-08 14:27:50 +0800

    add config module

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,7 +3,6 @@ import { toNumber } from 'lodash';
 import { createAppConfig } from '@/modules/core/config';
 
 export const app = createAppConfig((configure) => ({
-        port: configure.env.get<number>('APP_PORT', (v) => toNumber(v), 3099),
-        prefix: 'api',
-    });
-});
+    port: configure.env.get<number>('APP_PORT', (v) => toNumber(v), 3099),
+    prefix: 'api',
+}));
