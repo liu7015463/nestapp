5c9ecec8fceda12ea2d358adb214c69777af9bb2
liuyi <liuyiinhhu@gmail.com>
2025-06-23 07:36:58 +0800

    add user module and jwt

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -2,8 +2,10 @@ import { Configure } from '@/modules/config/configure';
 import { ConfigureFactory } from '@/modules/config/types';
 import * as contentControllers from '@/modules/content/controllers';
 import { ApiConfig, VersionOption } from '@/modules/restful/types';
+import { createUserApi } from '@/modules/user/routes';
 
 export const v1 = async (configure: Configure): Promise<VersionOption> => {
+    const userApi = createUserApi();
     return {
         routes: [
             {
@@ -17,6 +19,7 @@ export const v1 = async (configure: Configure): Promise<VersionOption> => {
                         { name: '标签操作', description: '对标签进行CRUD操作' },
                         { name: '文章操作', description: '对文章进行CRUD操作' },
                         { name: '评论操作', description: '对评论进行CRUD操作' },
+                        ...userApi.tags.app,
                     ],
                 },
                 children: [
@@ -25,6 +28,7 @@ export const v1 = async (configure: Configure): Promise<VersionOption> => {
                         path: 'content',
                         controllers: Object.values(contentControllers),
                     },
+                    ...userApi.routes.app,
                 ],
             },
         ],
